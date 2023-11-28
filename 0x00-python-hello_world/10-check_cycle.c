f61b5deb6dc112e6679e78d43231c013a925d689
mohamedali382 <ma392213@gmail.com>
2023-11-27 16:42:54 -0800

    J

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include"lists.h"
+
+/**
+ * check_cycle - check if this list is cycle
+ * @list: the list to be checked
+ * Return: 0 or 1
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *first, *second;
+
+	second = first;
+	first = list;
+
+	while (first && first->next && second->next)
+	{
+		second = second->next;
+		first = first->next;
+		if (first = second)
+			return(1);
+	}
+	return(0);
+}
