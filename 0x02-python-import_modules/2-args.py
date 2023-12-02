945a3de8087cc1a3025d436dfdcb8fb29507b0cc
mohamedali382 <ma392213@gmail.com>
2023-12-01 17:00:20 -0800

    J

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    print(len(argv) - 1, end=" ")
+    if len(argv) == 2:
+        print("arguments:")
+    else:
+        print("arguments", end="")
+        if len(argv) == 1:
+            print(".")
+        else:
+            print(":")
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
